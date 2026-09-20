c562c46bd4da55f912a7f4449c6788e05fd8a49a
Usman Iqbal <usmaniqbal036@gmail.com>
2026-09-20 12:20:01 +0500

    Fix Mongoose pre-save hook - remove unsupported next() in async middleware

--- Back-end/models/User.js
+++ Back-end/models/User.js
@@ -26,11 +26,10 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
+userSchema.pre('save', async function () {
+  if (!this.isModified('password')) return;
 
   this.password = await bcrypt.hash(this.password, 12);
-  next();
 });
 
 userSchema.methods.matchPassword = function (password) {
